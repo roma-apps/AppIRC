4605d488d5998d7410101f059f05f5eefbb179e8
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2019-10-24 17:59:15 +0300

    add isolates in url finder

--- lib/url/url_finder.dart
+++ lib/url/url_finder.dart
@@ -1,21 +1,22 @@
+import 'package:flutter/foundation.dart';
 
-   final _regex = RegExp(
-    r"(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z0-9]{2,6}\b"
-    r"([-a-zA-Z0-9@:%_\+.~#?&//=]*)",
-  );
-// final _regex = RegExp(
-//  r"(www|http:|https:)+[^\s]+[\w]",
-//);
+final _regex = RegExp(
+  r"(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z0-9]{2,6}\b"
+  r"([-a-zA-Z0-9@:%_\+.~#?&//=]*)",
+);
 
 Future<List<String>> findUrls(List<String> originalTexts) async {
+  return await compute(_findUrls, originalTexts);
+}
+
+List<String> _findUrls(List<String> originalTexts) {
   var resultUrls = <String>[];
 
-  // TODO: improve performance with isolates
   for (var value in originalTexts) {
-    if(value == null) {
+    if (value == null) {
       continue;
     }
-    var urlMatches  = _regex.allMatches(value);
+    var urlMatches = _regex.allMatches(value);
 
     for (var urlMatch in urlMatches) {
       var url = urlMatch.group(0);
@@ -23,7 +24,6 @@ Future<List<String>> findUrls(List<String> originalTexts) async {
         resultUrls.add(url);
       }
     }
-
   }
 
   return resultUrls;
