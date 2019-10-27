d88fe2a397d15ff537f32404dc842a37ea1441e8
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2019-10-27 14:26:44 +0300

    refactor disposable owner

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -1,51 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/widgets.dart';
-import 'package:flutter_appirc/disposable/async_disposable.dart';
-import 'package:flutter_appirc/disposable/disposable.dart';
-import 'package:flutter_appirc/disposable/rx_disposable.dart';
-import 'package:flutter_appirc/disposable/ui_disposable.dart';
-import 'package:rxdart/rxdart.dart';
-
-class DisposableOwner extends Disposable {
-  bool disposed = false;
-  final CompositeDisposable _compositeDisposable = CompositeDisposable([]);
-
-  void addDisposable(
-      {Disposable disposable,
-      StreamSubscription streamSubscription,
-      TextEditingController textEditingController,
-      Subject subject,
-      Timer timer}) {
-    if (disposable != null) {
-      _compositeDisposable.children.add(disposable);
-    }
-
-    if (subject != null) {
-      _compositeDisposable.children.add(SubjectDisposable(subject));
-    }
-
-    if (timer != null) {
-      _compositeDisposable.children.add(TimerDisposable(timer));
-    }
-
-    if (streamSubscription != null) {
-      _compositeDisposable.children
-          .add(StreamSubscriptionDisposable(streamSubscription));
-    }
-
-    if (textEditingController != null) {
-      _compositeDisposable.children
-          .add(TextEditingControllerDisposable(textEditingController));
-    }
-  }
-
-  @mustCallSuper
-  void dispose() {
-    disposed = true;
-    _compositeDisposable.dispose();
-  }
-}
+import 'package:flutter_appirc/disposable/disposable_owner.dart';
 
 abstract class Providable extends DisposableOwner {}
 
