b1504f4ab4e690419a4061b650fb11365b0cb98c
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2019-11-03 17:02:21 +0300

    add progress indicator for image preview

--- lib/app/message/preview/message_image_preview_widget.dart
+++ lib/app/message/preview/message_image_preview_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_appirc/app/message/preview/message_preview_model.dart';
 
@@ -7,5 +8,19 @@ Widget buildMessageImagePreview(
 }
 
 Widget buildPreviewImageThumb(BuildContext context, String thumb) {
-  return Image.network(thumb);
+  return Image.network(
+    thumb,
+    loadingBuilder:
+        (BuildContext context, Widget child, ImageChunkEvent loadingProgress) {
+      if (loadingProgress == null) return child;
+      return Center(
+        child: CircularProgressIndicator(
+          value: loadingProgress.expectedTotalBytes != null
+              ? loadingProgress.cumulativeBytesLoaded /
+                  loadingProgress.expectedTotalBytes
+              : null,
+        ),
+      );
+    },
+  );
 }
