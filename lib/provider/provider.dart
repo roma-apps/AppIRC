fffc43ac95ec2174080ccf528ee0bdc1441588af
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-02-20 14:45:58 +0300

    fix provider: don't use deprecated method

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -17,12 +17,9 @@ class Provider<T extends Providable> extends StatefulWidget {
   _ProviderState<T> createState() => _ProviderState<T>();
 
   static T of<T extends Providable>(BuildContext context) {
-    final type = _typeOf<Provider<T>>();
-    Provider<T> provider = context.ancestorWidgetOfExactType(type);
+    Provider<T> provider = context.findAncestorWidgetOfExactType<Provider<T>>();
     return provider.providable;
   }
-
-  static Type _typeOf<T>() => T;
 }
 
 class _ProviderState<T> extends State<Provider<Providable>> {
