c56f117692722980c101353a58196c4ecc3bfed7
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-02-27 22:31:28 +0300

    make 'https://irc.pleroma.social/' pre-filled instance uri

--- lib/app/default_values.dart
+++ lib/app/default_values.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_appirc/lounge/lounge_model.dart';
 
-// const String appIRCLoungeInstance = "https://irc.pleroma.social/";
-const String appIRCLoungeInstance = "http://973099570f9d.ngrok.io";
+const String appIRCLoungeInstance = "https://irc.pleroma.social/";
+// const String appIRCLoungeInstance = "http://973099570f9d.ngrok.io";
 // const String appIRCLoungeInstance = "https://demo.thelounge.chat/";
 
 LoungePreferences createDefaultLoungePreferences() => LoungePreferences(
