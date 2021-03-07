2007faf15a02e4fe4bcb9f5e6037be24819d93da
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-03-07 20:41:21 +0300

    default_values.dart : irc.pleroma.social

--- lib/app/default_values.dart
+++ lib/app/default_values.dart
@@ -1,10 +1,6 @@
 import 'package:flutter_appirc/lounge/lounge_model.dart';
 
-// const String appIRCLoungeInstance = "https://irc.pleroma.social/";
-// const String appIRCLoungeInstance = "http://161.35.139.75:9103/";
-// const String appIRCLoungeInstance = "http://161.35.139.75:9103/";
-const String appIRCLoungeInstance = "http://bd6ea68583c5.ngrok.io";
-// const String appIRCLoungeInstance = "https://demo.thelounge.chat/";
+const String appIRCLoungeInstance = "https://irc.pleroma.social/";
 
 LoungePreferences createDefaultLoungePreferences() => LoungePreferences(
       hostPreferences: LoungeHostPreferences(
